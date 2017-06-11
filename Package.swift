ddd0ebb3500f45f1e09fb054ca2a89480df2c45f
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-11 16:32:03 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CSDL2",
     pkgConfig: "sdl2",
-    providers: [.Brew("sdl2"), .Apt("libsdl2-dev")]
+    providers: [.brew(["sdl2"]), .apt(["libsdl2-dev"])]
 )
