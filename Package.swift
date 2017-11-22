7cfe34d9d12cf7fe958c42c2f37577bd01c703e4
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-22 04:20:50 -0500

    Fixed for Swift 3.0.2 compatibility

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,5 +6,5 @@ import PackageDescription
 let package = Package(
     name: "CSDL2",
     pkgConfig: "sdl2",
-    providers: [.brew(["sdl2"]), .apt(["libsdl2-dev"])]
+    providers: [.Brew("sdl2"), .Apt("libsdl2-dev")]
 )
